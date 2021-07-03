27b0d43c49b230e95caa5ca815d514197e054577
David Wei <yunze.wei@edu.sait.ca>
2021-07-02 21:39:28 -0600

    First Commit -Set up the file structure. -ConnectionPool / DBUtil in dataaccess is done. (same as the demo) -Added to context.xml (double check it to see if its right) -Added empty method for RoleDB -Imported JSTL lib and added Lib tag for JSP

---
+++ src/java/dataaccess/RoleDB.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import java.util.List;
+import models.Role;
+
+public class RoleDB {
+
+        public List<Role> getAll(String role) throws Exception{
+           
+            // to be deleted once the method is filled in
+            return null;
+        }
+}
