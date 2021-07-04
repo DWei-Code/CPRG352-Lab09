028fb03558113d7123a712f3011bcf421ee5d69a
David Wei <yunze.wei@edu.sait.ca>
2021-07-04 15:53:11 -0600

    Second Commit Added Role/user models

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,5 +1,35 @@
 package models;
 
-public class Role {
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+    private int roleId;
+    private String roleName;
+
+    public Role() {
+
+    }
+
+    public Role(int roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 
 }
