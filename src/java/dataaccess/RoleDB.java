6213a0219b77659c3bfc9f5bc3eed2dbb6ca7fb6
840979 <840979@SAIT227580IT.ACDM.DS.SAIT.CA>
2021-07-09 06:40:11 -0600

    dataaccess- getAll()method finished - IS

--- src/java/dataaccess/RoleDB.java
+++ src/java/dataaccess/RoleDB.java
@@ -1,5 +1,9 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import models.Role;
 
@@ -9,4 +13,31 @@ public class RoleDB {
             // to be deleted once the method is filled in
             return null;
         }
+        
+        public List <Role> getAll(String owner) throws Exception {
+        List<Role> roles = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT role_id, role_name from role";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int roleId = rs.getInt(1);
+                String roleName = rs.getString(2);
+                Role role = new Role(roleId, roleName);
+                roles.add(role);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+
+        return roles;
+    }
 }
